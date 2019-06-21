43d2397592829dde2e1ecd2db73041672d235ee1
Amy Westcott <amy.westcott@yahoo.com>
2019-06-21 21:19:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,8 +6,9 @@ class CashRegister
     self.items = []
     self.last_transaction = {}
   end
-  def add_item(title, price)
-    self
-  end 
+  def add_item(item, price, quantity = 1)
+    quantity.times do
+      self.items << item
+    end
 end 
 
